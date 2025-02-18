008577bcab77a1af3c6a00ba85214bf644c6ad8e
ryanbai2008 <69367510+ryanbai2008@users.noreply.github.com>
2025-02-18 12:31:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -495,21 +495,26 @@ while not drone_1_terminate and not drone_2_terminate:
         sleep_time = time.time() - timer
     
     if not drone_1_terminate:
-        drone_1_pos[0] += drone_1_movement[0] * sleep_time
-        drone_1_pos[1] += drone_1_movement[1] * sleep_time
-        drone_1_pos[2] += turn_1 * sleep_time
-        drone_1_pos[2] = drone_1_pos[2] % 360
+        drone_1_pos[2] = drone1.get_yaw()
+        theta_x_component_1 = (drone_1_pos[2] - 90 * (drone_1_pos[0] > 0)) % 360
+        theta_y_component_1 = (drone_1_pos[2] + 180) % 360
+        delta_x_1 = abs(drone_1_movement[0]) * math.cos(math.radians(theta_x_component_1)) + abs(drone_1_movement[1]) * math.cos(math.radians(theta_y_component_1))
+        delta_y_1 = abs(drone_1_movement[0]) * math.sin(math.radians(theta_x_component_1)) + abs(drone_1_movement[1]) * math.sin(math.radians(theta_y_component_1))
+        drone_1_pos[0] += delta_x_1 * sleep_time
+        drone_1_pos[1] += delta_y_1 * sleep_time
     else:
-        drone_1_pos[2] += turn_1 * sleep_time
-        drone_1_pos[2] = drone_1_pos[2] % 360
+        drone_1_pos[2] = drone1.get_yaw()
+
     if not drone_2_terminate:   
-        drone_2_pos[0] += drone_2_movement[0] * sleep_time
-        drone_2_pos[1] += drone_2_movement[1] * sleep_time
-        drone_2_pos[2] += turn_2 * sleep_time
-        drone_2_pos[2] = drone_2_pos[2] % 360
+        drone_2_pos[2] = drone2.get_yaw()
+        theta_x_component_2 = (drone_2_pos[2] - 90 * (drone_2_pos[0] > 0)) % 360
+        theta_y_component_2 = (drone_2_pos[2] + 180) % 360
+        delta_x_2 = abs(drone_2_movement[0]) * math.cos(math.radians(theta_x_component_2)) + abs(drone_2_movement[1]) * math.cos(math.radians(theta_y_component_2))
+        delta_y_2 = abs(drone_2_movement[0]) * math.sin(math.radians(theta_x_component_2)) + abs(drone_2_movement[1]) * math.sin(math.radians(theta_y_component_2))
+        drone_2_pos[0] += delta_x_2 * sleep_time
+        drone_2_pos[1] += delta_y_2 * sleep_time
     else:
-        drone_2_pos[2] += turn_2 * sleep_time
-        drone_2_pos[2] = drone_2_pos[2] % 360
+        drone_2_pos[2] = drone2.get_yaw()
 
     if intersection:
         if(drone2.getHeight() >= drone1.getHeight() + 20):
